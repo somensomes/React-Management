33f6391ba459a73a2818096f36a74711ebb2369e
somensomes <somensomes@gmail.com>
2021-03-09 17:53:40 +0900

    Creat Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's Develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/264/264/1',
+    'name': '홍길겸',
+    'birthday': '951212',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/264/264/2',
+    'name': '홍길순',
+    'birthday': '951212',
+    'gender': '여자',
+    'job': '프로그래머'
+  },
+
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/264/264/3',
+    'name': '홍길방',
+    'birthday': '951212',
+    'gender': '남자',
+    'job': '요리사'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id = {c.id}
+              image = {c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+              );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
