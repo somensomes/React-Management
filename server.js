396312790c28051892a0230225adf9d51bb34385
somensomes <somensomes@DESKTOP-8CB2P0K>
2021-03-10 16:14:15 +0900

    Create CUstomer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,36 +6,26 @@ const port = process.env.PORT || 5454;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send(
-        [
-            {
-              'id': 1,
-              'image': 'https://placeimg.com/164/164/1',
-              'name': '홍길겸',
-              'birthday': '951212',
-              'gender': '남자',
-              'job': '대학생'
-            },
-            {
-              'id': 2,
-              'image': 'https://placeimg.com/164/164/2',
-              'name': '홍길순',
-              'birthday': '951212',
-              'gender': '여자',
-              'job': '프로그래머'
-            },
-          
-            {
-              'id': 3,
-              'image': 'https://placeimg.com/164/164/3',
-              'name': '홍길방',
-              'birthday': '951212',
-              'gender': '남자',
-              'job': '요리사'
-            }
-          ]
-    )
-})
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+    res.send(rows);
+    }
+);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
